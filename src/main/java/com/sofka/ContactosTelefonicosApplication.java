936de89caf6cc593b0dd29f6f3e5cc3cf992c3d5
Oscar Martinez <oscar_mar307@outlook.com>
2023-04-13 18:27:09 -0500

    Conexión a la base de datos exitosa.

---
+++ src/main/java/com/sofka/ContactosTelefonicosApplication.java
@@ -0,0 +1,13 @@
+package com.sofka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactosTelefonicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactosTelefonicosApplication.class, args);
+	}
+
+}
